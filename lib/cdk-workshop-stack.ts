171b069bf8bb20fe90a079e19a017e717bed587b
ot-nemoto <41175383+ot-nemoto@users.noreply.github.com>
2024-03-09 12:45:12 +0000

    コンストラクトを書く
    ref https://cdkworkshop.com/ja/20-typescript/40-hit-counter.html

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,21 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // defines an AWS Lambda resource
         const hello = new lambda.Function(this, 'HelloHandler', {
-            runtime: lambda.Runtime.NODEJS_20_X,
+            runtime: lambda.Runtime.NODEJS_16_X,
             code: lambda.Code.fromAsset('lambda'),
             handler: 'hello.handler',
         });
 
-        // defines an API Gateway REST API resource backed by our "hello" function.
+        const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+            downstream: hello,
+        });
+
         new apigw.LambdaRestApi(this, 'Endpoint', {
-            handler: hello,
+            handler: helloWithCounter.handler,
         });
     }
 }
